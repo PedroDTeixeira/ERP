4f249c155a681e783709196168e78ebe0920421b
Pedro Teixeira <pedroteixeira091@gmail.com>
2022-08-26 21:04:34 +0200

    Grids, input clear fail, styling

---
+++ script.js
@@ -0,0 +1,9 @@
+const submitButton = document.querySelector('#submit');
+
+// Attempt to clear the input text when Submitting the button
+
+submitButton.addEventListener('click', () => {
+    const testDiv = document.querySelector('.wrapper2');
+    const huahua = testDiv.querySelectorAll(':scope > div');
+
+})
