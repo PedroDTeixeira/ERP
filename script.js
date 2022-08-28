6e4aada2b1e4066953715daf907997fbb9cd12fd
Pedro Teixeira <pedroteixeira091@gmail.com>
2022-08-28 23:11:03 +0200

    Adding amounts to the object changes

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 const submitButton = document.querySelector('#submit');
-// const amountAccount = document.querySelector('#accCode1');
 
 let accounts = ['0001000']
-let accountsBalance = [1,3]
+let accountsBalance = []
 
-// Adding amounts to the object attempt
+// Adding amounts to the object
 submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const amountAccount = +document.querySelector('#accCode1').value;
-    accountsBalance.push(amountAccount);
-    console.log(accountsBalance);
-    document.querySelector('#accCode1').value = '';
-    const totalSummarize = summarize();
-    console.log(totalSummarize);
+  e.preventDefault();
+  const accountCode = document.querySelector('#accCode1').value;
+  let jeAmount =  document.querySelector('#journalAmount').value;
+
+  if (!accountCode || !jeAmount) {
+    console.log('Account number or amount missing');
+  } else {
+    if (accounts.includes(accountCode) === true) {    
+      const amountAccount = +jeAmount;
+      accountsBalance.push(amountAccount);
+      console.log(accountsBalance);
+      jeAmount = '';
+      const totalSummarize = summarize();
+      console.log(totalSummarize);
+    } else {
+      console.log(`Account ${accountCode} is not in the chart of accounts.`);
+    }
+  }
 })
 
 // Function that summarize the array
 function summarize() {
-const initialValue = 0;
-const sumWithInitial = accountsBalance.reduce(
-  (previousValue, currentValue) => previousValue + currentValue,
-  initialValue)
+  const initialValue = 0;
+  const sumWithInitial = accountsBalance.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue);
   return sumWithInitial;
 };
-
