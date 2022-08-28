753a09320ec88ed4dc2cc2bacf9e92b6eb2ab74c
Pedro Teixeira <pedroteixeira091@gmail.com>
2022-08-28 19:17:40 +0200

    Function that summarize the array

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const submitButton = document.querySelector('#submit');
 // const amountAccount = document.querySelector('#accCode1');
 
 let accounts = ['0001000']
-let accountsBalance = []
+let accountsBalance = [1,3]
 
 // Adding amounts to the object attempt
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
-    const amountAccount = document.querySelector('#accCode1').value;
+    const amountAccount = +document.querySelector('#accCode1').value;
     accountsBalance.push(amountAccount);
     console.log(accountsBalance);
     document.querySelector('#accCode1').value = '';
-    console.log(sumWithInitial);
+    const totalSummarize = summarize();
+    console.log(totalSummarize);
 })
 
+// Function that summarize the array
+function summarize() {
 const initialValue = 0;
 const sumWithInitial = accountsBalance.reduce(
   (previousValue, currentValue) => previousValue + currentValue,
-  initialValue
-);
+  initialValue)
+  return sumWithInitial;
+};
 
