e341263654f18c74812a05213f25cd8b0dce6840
Pedro Teixeira <pedroteixeira091@gmail.com>
2022-08-28 13:35:51 +0200

    Attempt 2 to sum a BS account

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const submitButton = document.querySelector('#submit');
-const amountAccount = document.querySelector('#accCode1').value;
+// const amountAccount = document.querySelector('#accCode1');
 
-let accounts = {account: 1}
-console.log(amountAccount)
+let accounts = ['0001000']
+let accountsBalance = []
+
 // Adding amounts to the object attempt
-submitButton.addEventListener('click', () => {
-    accounts.account = accounts.account + amountAccount || 1;
-    console.log(accounts.account)
-    document.querySelector('#accCode1').value = ''
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const amountAccount = document.querySelector('#accCode1').value;
+    accountsBalance.push(amountAccount);
+    console.log(accountsBalance);
+    document.querySelector('#accCode1').value = '';
+    console.log(sumWithInitial);
 })
+
+const initialValue = 0;
+const sumWithInitial = accountsBalance.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+
