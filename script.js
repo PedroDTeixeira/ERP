9148ac87920159a0f0520bf9a068f9926a8f9592
Pedro Teixeira <pedroteixeira091@gmail.com>
2022-09-04 13:42:10 +0200

    Journal entry history with acc and amount

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const submitButton = document.querySelector('#submit');
 const clearButton = document.querySelector('#clearSubmit');
-const journalInformation = document.querySelectorAll('#accCode1, #accDesc, #journalAmount, #accCode, #accDesc, #journalAmount2');
 const accountCode1 = document.querySelector('#accCode1').value;
 const accountCode2 = document.querySelector('#accCode2').value;
+const journalInformation = document.querySelectorAll('#accCode1, #accDesc, #journalAmount, #accCode2, #accDesc, #journalAmount2');
 
 let accounts = ['0001000']
 let accountsBalance = []
@@ -10,10 +10,12 @@ let accountsBalance = []
 // Adding amounts to the object
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
+  const accountCode1 = document.querySelector('#accCode1').value;
+  const accountCode2 = document.querySelector('#accCode2').value;
   let jeAmount1 = +document.getElementById('journalAmount').value;
   let jeAmount2 = +document.getElementById('journalAmount2').value;
   if (checkJournalNetZero() != 'JE not zero') {
-    if (!accountCode1 ||!accountCode2 || !jeAmount1 || !jeAmount2) {
+    if (!accountCode1 || !accountCode2 || !jeAmount1 || !jeAmount2) {
       console.log('Account number or amount missing');
     } else {
       if (accounts.includes(accountCode1) === true || accounts.includes(accountCode1) === true) {
@@ -37,22 +39,29 @@ function summarize() {
   return sumWithInitial;
 };
 
+// What: Creates the journal entry details Why: All record history must to stored for accounting and analysis purposes
 function addElement() {
   // create a new div element
-  const newDiv = document.createElement("li");
+  const newDiv1 = document.createElement("li");
+  const newDiv2 = document.createElement("li");
 
   // and give it some content
+  const accountCode1 = document.querySelector('#accCode1').value;
+  const accountCode2 = document.querySelector('#accCode2').value;
   let jeAmount1 = +document.getElementById('journalAmount').value;
   let jeAmount2 = +document.getElementById('journalAmount2').value;
-  const newContent = document.createTextNode(`${jeAmount1} ${accountCode1}`);
+  const newContent1 = document.createTextNode(`Account ${accountCode1} Amount ${jeAmount1}`);
+  const newContent2 = document.createTextNode(`Account ${accountCode2} Amount ${jeAmount2}`);
 
   // add the text node to the newly created li
-  newDiv.appendChild(newContent);
+  newDiv1.appendChild(newContent1);
+  newDiv2.appendChild(newContent2);
 
   // add the newly created element and its content into the DOM
   const currentDiv = document.getElementById("ulList");
   const currentDiv2 = document.getElementById("ulList2");
-  currentDiv.insertBefore(newDiv, currentDiv2);
+  currentDiv.insertBefore(newDiv1, currentDiv2);
+  currentDiv.insertBefore(newDiv2, currentDiv2);
 }
 
 // Checking if amounts are equal to zero (debits and credits have to net to zero)
